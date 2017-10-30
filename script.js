280f46dc51d87e2a6a7ad75260a5d49b6c4de3fc
Ashley Amato <ashleyamato1@gmail.com>
2017-10-30 11:17:08 -0600

    add markers successfully

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 function initMap() {
-  let options = {
-    zoom: 8,
-    center: {lat:40.0150, lng:-105.2705}
-  }
-
-  let map = new google.maps.Map(document.getElementById('map'), options)
 }
-
-$(document).ready(function(){
-    $('.parallax').parallax();
   let $xhr = $.ajax ({
     url: 'https://developer.nps.gov/api/v1/parks?api_key=uRy7DBPiQRPP86xvn1XwEtfKNl9CQxVEEqkAKbSr',
     type: 'GET',
     dataType: 'json',
     success: function(data) {
-      var markers = []
+      var options = {
+        zoom: 3,
+        center: {lat:40.0150, lng:-105.2705}
+      }
+
+      var map = new google.maps.Map(document.getElementById('map'), options)
       console.log(data.data)
       for (let i = 0; i < data.data.length; i++) {
         if (data.data[i].latLong === '') {
@@ -22,23 +18,14 @@ $(document).ready(function(){
         var latLongStr = data.data[i].latLong
         var lat = latLongStr.slice(4, 13)
         var lng = latLongStr.slice(22, 30)
-        var latLongObj = {}
-        latLongObj.lat = parseFloat(lat)
-        latLongObj.lng = parseFloat(lng)
-        markers.push(latLongObj)
+
+        var latLng = new google.maps.LatLng(lat, lng)
+        var marker = new google.maps.Marker({
+          position: latLng,
+        })
+        marker.setMap(map)
         }
       }
-      function addMarker(coords){
-        var marker = new google.maps.Marker({
-          position: coords,
-          setMap: map,
-        })
-      }
-      addMarker({lat:40.0150, lng:-105.2705})
-      // console.log('markers', markers)
-      // for (let i = 0; i < markers.length; i++) {
-      //   addMarker(markers[i])
-      // }
     },
     error: function() {alert('failed')},
   })
@@ -50,4 +37,3 @@ $(document).ready(function(){
         newPos = scrollPos+scrollIncrement;
     $window.scrollTop( newPos );
 } );
-})
